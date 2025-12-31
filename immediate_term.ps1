2281727c46f1b169529be7cc9ea7a6e33de86e8a
Keith <8584184+git-kv@users.noreply.github.com>
2025-12-31 10:09:12 -0600

    fixed looping

--- immediate_term.ps1
+++ immediate_term.ps1
@@ -1,6 +1,6 @@
 # Scheduled to check a folder every minute to see if there is an
 # immediate termination waiting to be processed.
-$root_path = "C:\Users\KVoelker\repos\immediate_account_disablement\"
+$root_path = "\\eocservices\apps$\programs\scripts\separation\immediate_account_disablement\"
 $smtp_server = "exnode2.dartadvantage.com"
 $from = "ImmediateTerm@dart.net"
 
@@ -17,18 +17,25 @@ while ($true) {
             $default_subject = "Account Disabled $user_to_be_disabled"
             $default_body = "$user_to_be_disabled's account has been disabled."
             $default_recipients = "kvoelker@dart.net"
+            $disable_account = $false
 
             Get-ADPrincipalGroupMembership $user_to_be_disabled | Select-Object Name | ForEach-Object {
                 if (($_.Name -eq "T2_Admins") -or ($_.Name -like "T2_VIP") -or ($_.Name -like "T1*") -or ($_.Name -like "T0*") -or ($_.Name -like "*Domain Admins")) {
                     Send-MailMessage -Subject $vip_subject -Body $vip_body -To $vip_recipients -From $from -SmtpServer $smtp_server
+                    $disable_account = $false
                 }
                 else {
-                    Disable-ADAccount -Identity $user_to_be_disabled
-                    New-Item "\\eocservices\apps$\programs\scripts\active_directory\needsync\sync_plz"
-                    Send-MailMessage -Subject $default_subject -Body $default_body -To $default_recipients -From $from -SmtpServer $smtp_server
+                    $disable_account = $true
                 }
             }
-            Remove-Item $_
+
+            if ($disable_account) {
+                Disable-ADAccount -Identity $user_to_be_disabled
+                New-Item "\\eocservices\apps$\programs\scripts\active_directory\needsync\sync_plz"
+                Send-MailMessage -Subject $default_subject -Body $default_body -To $default_recipients -From $from -SmtpServer $smtp_server
+            }
+
+            Remove-Item $_.FullName
         }
     }
     Start-Sleep -Seconds 60
